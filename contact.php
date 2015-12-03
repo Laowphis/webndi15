8c299b31aa73741b1f70c8a057c58bdca5a29618
Laowphis <john.fernandes89@gmail.com>
2015-12-03 22:10:48 +0100

    contact+jpg

---
+++ contact.php
@@ -0,0 +1,12 @@
+<body>
+
+	
+	<div id="content">
+		<h2>Membre de l'équipe:</h2>
+		<p><img src="team.jpg" alt="teamXID"/></p>
+		<p>Chef d'équipe: Galliere Mathieu</br>
+		collaborateur: Renier Dimitri, Szymanski Cedric, Lazzaro Daniel, Jacob Phillipe, Rat Joris, Alberola Gael, Fernandes Jonathan, Jacquey Axel </br>
+		</p>
+	</div>
+	
+</body>
