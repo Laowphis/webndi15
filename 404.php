e49b494478d560a3ec7d89f4da885ccf84d378d9
Laowphis <john.fernandes89@gmail.com>
2015-12-03 22:00:27 +0100

    content+redbutton v2

---
+++ 404.php
@@ -0,0 +1,8 @@
+<body>
+
+	
+	<div id="content">
+		<p>Erreur 404</p>
+	</div>
+	
+</body>
--- content.php
+++ content.php
@@ -1,13 +1,12 @@
 <body>
 
-	<h1 id="title">PanicButton</h1>
 	
 	<div id="content">
 		<p>Notre application smartphone permet de savoir si votre vie ou santé est menacée notamment en cas de conflits armés, d'épidémies,
-de pandémies, de catastrophes naturelles, d'attentats terroristes.  </p>
-		<p>Pour cela il vous suffira simplemnt de téléchargger notre application a l'adrrese suivante: <a href="lien de telechargement"> <img src="red-button" alt="redbutton"/> </a></p>
+de pandémies, de catastrophes naturelles, d'attentats.  </p>
+		<p>Pour cela il vous suffira simplemnt de téléchargger notre application a l'adrrese suivante: <a href="lien de telechargement"> <img src="red-button.png" alt="redbutton"/> </a></p>
 		<p>Lors que vous sentez que votre vie est menacée il vous suffira de sortire votre application de la poche et d'appuyer sur le gros bouton rouge. Ceci signalera un problème à notre base de donnée.</p>
-		
+		<p>image appli plus description du fonctionnenment appli.</p>
 		<!--coodronée gps envoyer-->
 	</div>
 	
