a39346cc39eed11f1054e3d7715f0d65e82b86da
Laowphis <john.fernandes89@gmail.com>
2015-12-04 06:47:38 +0100

    sjy

--- telechargement.php
+++ telechargement.php
@@ -3,5 +3,5 @@
 <div class="list-group">
   <a href="panicbutton_server.jar" class="list-group-item">Téléchargement Serveur</a>
   <a href="panicbutton.apk" class="list-group-item">Téléchargement Client</a>
-  <a href="XID_NDLI_FINAL.pdf" class="list-group-item">Power Point presentation</a>
+  <a href="XID_NDLI_Final.pdf" class="list-group-item">Power Point presentation</a>
 </div>
