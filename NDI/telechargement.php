9b7d193be2b29425e6c757694fbd33db3a44af9b
Laowphis <john.fernandes89@gmail.com>
2015-12-04 06:35:16 +0100

    maj

--- NDI/telechargement.php
+++ NDI/telechargement.php
@@ -2,6 +2,6 @@
 	
 <div class="list-group">
   <a href="panicbutton_server.jar" class="list-group-item">Téléchargement Serveur</a>
-  <a href="panicbutton.pk" class="list-group-item">Téléchargement Client</a>
+  <a href="panicbutton.apk" class="list-group-item">Téléchargement Client</a>
   <a href="XID_NDLI_FINAL.pdf" class="list-group-item">Power Point presentation</a>
 </div>
