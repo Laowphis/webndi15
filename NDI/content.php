afcc0f7a012e2de3adab3bbcb3dc98c31f6249f5
Laowphis <john.fernandes89@gmail.com>
2015-12-04 05:55:28 +0100

    image appli

--- NDI/content.php
+++ NDI/content.php
@@ -5,7 +5,7 @@
 de pandémies, de catastrophes naturelles, d'attentats.  </p>
 		<p>Pour cela il vous suffira simplement de télécharger notre application à l'adresse suivante: </br><a id="button" href="index.php?page=telechargement"> <img id="button" src="red-button.png" alt="redbutton"/> </a></p>
 		<p>Lorsque vous sentez que votre vie est menacée il vous suffira de sortir votre application de la poche et d'appuyer sur le gros bouton rouge. Ceci signalera un problème aux autorités compétente.</p>
-		<p>image appli.</p>
+		<p><img src="screen_framed.png" alt="screen"/>.</p>
 		<p>Fonctionnement de l'application:</br>
 			Le signal de détresse envera vos coordonée gps aux autorités compétentes (police, gendarmerie, pompier, ...) qui traiteront votre alerte.
 			Si une cinquantaine de personnes envoient une alerte simultanement venant d'un même endroit, les forces de l'ordre pourront être prévenue.
